2ca1dc97f07e670b1a8ccce840674adf2a577299
Keannu Bernasol <noehbernasol0@gmail.com>
2024-10-17 16:23:05 +0800

    Update production url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://resources.06222001.xyz",
+  url: "https://blog.06222001.xyz",
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
